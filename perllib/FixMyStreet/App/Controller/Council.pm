d0f740bf6a0fffb89ba7e9ab20314f8015d9343f
martanducas <72607737+martanducas@users.noreply.github.com>
2025-04-11 17:46:47 +0200

    Revert "FMSK - edits to text and translations 2025"
    This reverts commit 01abea1cd79f1bf809e523d58b51d498eb6cd2fe.

--- perllib/FixMyStreet/App/Controller/Council.pm
+++ perllib/FixMyStreet/App/Controller/Council.pm
@@ -99,7 +99,7 @@ sub load_and_check_areas : Private {
     # If we don't have any areas we can't accept the report
     if ( !scalar keys %$all_areas ) {
         $c->stash->{location_error_no_areas} = 1;
-        $c->stash->{location_error} = _('The selected location is outside of Kythera island. Please try again.');
+        $c->stash->{location_error} = _('That location does not appear to be covered by a council; perhaps it is offshore or outside the country. Please try again.');
         return;
     }
 
